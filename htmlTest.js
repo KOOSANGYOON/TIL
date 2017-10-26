afbf914c09c7d561e521ec85131885cd90263476
KOOSANGYOON <nevergiveup936@naver.com>
2017-10-26 17:19:20 +0900

    "day 9 수정"

---
+++ htmlTest.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    document.getElementById("demo").style.fontSize = "25px";
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "yellow";
+}
+
+
+var result = Math.ceil(100 * Math.random());
+var challengeNum = 1;
+console.log(result);
+
+document.write("게임을 시작합니다.<br>");
+
+while(1) {
+  var inputNum = parseInt(prompt("숫자를 입력해주세요."));
+  if (inputNum > result) {
+    document.write(challengeNum + " 번째 시도, 땡! " + inputNum + "보다 작은 값입니다.<br>");
+  }else if (inputNum < result) {
+    document.write(challengeNum + " 번째 시도, 땡! " + inputNum + "보다 큰 값입니다.<br>");
+  }else {
+    document.write(challengeNum + " 번째 시도, 딩동댕! 정답은 " + inputNum + "입니다!<br>");
+    break;
+  }
+  challengeNum++;
+}
+
+document.write("게임이 끝났습니다.<br>");
